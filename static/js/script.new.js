b5d808febeec9e0219a3948c1225571e1089dfb8
nikolay <magarinnikolay@gmail.com>
2019-03-31 13:17:23 +0300

    added several functions

--- static/js/script.new.js
+++ static/js/script.new.js
@@ -14,7 +14,7 @@ var score = 0,
 
 // Returns random num x: a <= x <= b
 function generate_random_number(a, b) {
-    var x = Math.round(Math.random() * (a - b) + a);
+    var x = Math.round(Math.random() * (b - a) + a);
     return x;
 }
 
@@ -26,12 +26,21 @@ function generate_random_operator() { // ==> String
 }
 
 
-function generate_wrong_answers(right_answer) { // ==> [Integer: x, Integer: y, Integer: z]
+function generate_answers_list(right_answer) { // ==> [Integer: x, Integer: y, Integer: z]
     var answers = [];
-    answers.push(right_answer);
-
-    // ...
-
+    var min_v = right_answer - 3;
+    var max_v = right_answer + 3;
+    while (answers.length < 4) {
+        let wrong_answer = generate_random_number(min_v, max_v);
+        if (answers.indexOf(wrong_answer) === -1) {
+            arr.push(wrong_answer);
+        }
+    }
+        if (answers.indexOf(right_answer) !== -1) {
+            return answers;
+        } else {
+            return generate_answers_list(right_answer);
+        }
     return answers;
 }
 
@@ -40,19 +49,29 @@ function generate_components(operator) {
 
     switch (operator) {
         case "+":
-            a = generate_random_number(7, 11);
-            b = generate_random_number(7, 11);
+            a = generate_random_number(1, 50);
+            b = generate_random_number(1, 50);
             answer = a + b;
             break;
         case "-":
-            a = generate_random_number(/* ... */);
-            b = generate_random_number(/* ... */);
+            a = generate_random_number(2, 90);
+            if (a > 10) {
+                b = generate_random_number(2, a-5);
+            } else {
+                b = generate_random_number(2, a);
+            }
             answer = a - b;
             break;
         case "×":
-            // ...
+            a = generate_random_number(2, 10);
+            b = generate_random_number(2, 10);
+            answer = a * b;
         case ":":
-            // ...
+            var res = generate_divide_components();
+            a = res[0];
+            b = res[1];
+            answer = res[2];
+            break;
     }
 
     return { a: a, b: b, answer: answer };
@@ -73,3 +92,13 @@ function generate_task() { // ==> Object: { a, b, operator, answer, answers_list
         answers_list: answers_list
     };
 }
+
+function generate_divide_components() {
+    a = generate_random_number(8, 100);
+    b = generate_random_number(4, 10);
+    while (Math.round(a / b) !== a / b) {
+        a++;
+    }
+    return [a, b, a/b]
+}
+
